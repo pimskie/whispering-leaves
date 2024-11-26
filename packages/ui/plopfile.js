136347d836f13c55bccca67f6c519b094ecc1803
Pim van Die <pim.vandie@iodigital.com>
2024-11-26 19:07:10 +0100

    added plop

--- packages/ui/plopfile.js
+++ packages/ui/plopfile.js
@@ -1,9 +1,5 @@
-import type { PlopTypes } from "@turbo/gen";
-
-// Learn more about Turborepo Generators at https://turbo.build/repo/docs/core-concepts/monorepos/code-generation
-
-export default function generator(plop: PlopTypes.NodePlopAPI): void {
-  // A simple generator to add a new React component to the internal UI library
+export default function (plop) {
+  // create your generators here
   plop.setGenerator("react-component", {
     description: "Adds a new react component",
     prompts: [
