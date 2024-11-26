850a88bc44c50da84ad88129814db0526d597894
Pim van Die <pim.vandie@iodigital.com>
2024-11-26 16:05:40 +0100

    fresh setup

---
+++ apps/foo/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@repo/ui/Button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button appName="Foo">Foo Button</Button>
+    </div>
+  );
+}
--- apps/netherlands/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from "@repo/ui/Button";
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Button appName="Netherlands2">Hello</Button>
-    </div>
-  );
-}
