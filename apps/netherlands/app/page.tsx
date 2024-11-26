85ae4231a8b7a21cc6a625a82b55f801b8453a42
Pim van Die <pim.vandie@iodigital.com>
2024-11-26 15:24:03 +0100

    netherlands app

--- apps/netherlands/app/page.tsx
+++ apps/netherlands/app/page.tsx
@@ -1,23 +1,8 @@
 import { Button } from "@repo/ui/Button";
-import { KeyStatistic } from "@repo/ui/KeyStatistic";
-import { TopicPill } from "@repo/ui/TopicPill";
-
 export default function Home() {
   return (
-    <div>
-      <Button appName="Netherlands">Button</Button>
-
-      <KeyStatistic
-        statistic={100}
-        description="Medewerkers overal ter wereld"
-        suffix="+"
-      ></KeyStatistic>
-
-      <TopicPill
-        title="Met liefde"
-        description="We maken apps en websites met passie en liefde"
-        iconName="Heart"
-      ></TopicPill>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Button appName="Netherlands2">Hello</Button>
     </div>
   );
 }
--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -11,7 +11,8 @@ function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  // stories: ["../../../packages/ui/**/*.stories.ts"],
+  stories: ["../**/*.stories.ts"],
   addons: [
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-essentials"),
